ddd4d380debd3eca5ff8255027ad6f4d32d57be6
tomori <tmrtkm1227tksx@gmail.com>
2022-01-21 00:31:25 +0900

    [add] Simple header file

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/21 00:27:08 by ttomori           #+#    #+#             */
+/*   Updated: 2022/01/21 00:29:34 by ttomori          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+
+#endif
