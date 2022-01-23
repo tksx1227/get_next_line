c4b0b32196d325c186e55916d7d8c562ce9f3eda
tomori <tmrtkm1227tksx@gmail.com>
2022-01-23 17:09:01 +0900

    [add] struct s_list

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,31 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 00:27:08 by ttomori           #+#    #+#             */
-/*   Updated: 2022/01/21 00:29:34 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/01/22 21:38:08 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# if 10000 < BUFFER_SIZE
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 512
+# endif
+
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
 # include <limits.h>
+# include <fcntl.h>
+
+typedef struct s_list
+{
+	int				fd;
+	char			*line;
+	char			*temp;
+	struct s_list	*next;
+}	t_list;
 
 char	*get_next_line(int fd);
 
