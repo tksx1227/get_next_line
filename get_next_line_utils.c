eb6c669a5672a501e4e2f49b765bfac7675d6d15
tomori <tmrtkm1227tksx@gmail.com>
2022-01-25 00:31:23 +0900

    [add] ft_strjoin function

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -58,3 +58,32 @@ char	*ft_strchr(const char *s, int c)
 	}
 	return (NULL);
 }
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	len1;
+	size_t	len2;
+	char	*p;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	p = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	i = 0;
+	while (i < len1)
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i < len2)
+	{
+		p[len1 + i] = s2[i];
+		i++;
+	}
+	p[len1 + len2] = '\0';
+	free(s1);
+	return (p);
+}
