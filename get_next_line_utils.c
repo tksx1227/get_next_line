eb323da000fe7d714e2e8dde875c6601708eae1c
tomori <tmrtkm1227tksx@gmail.com>
2022-01-25 00:08:16 +0900

    [add] ft_strchr function

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -43,3 +43,18 @@ char	*ft_strndup(const char *s, size_t size)
 	p[i] = '\0';
 	return (p);
 }
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	c = (char)c;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return ((char *)s + i);
+		i++;
+	}
+	return (NULL);
+}
