bd5cb654aefcf096a54daebf2a486c8db325d26d
tomori <tmrtkm1227tksx@gmail.com>
2022-01-25 09:51:50 +0900

    [add] gnl_read function

---
+++ get_next_line.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 00:19:56 by ttomori           #+#    #+#             */
+/*   Updated: 2022/01/25 09:51:35 by ttomori          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static int	gnl_read(int fd, char **line)
+{
+	ssize_t	rc;
+	char	*buf;
+
+	while (1)
+	{
+		buf = (char *)malloc(sizeof(char) * ((size_t)BUFFER_SIZE + 1));
+		rc = read(fd, buf, BUFFER_SIZE);
+		if (rc == -1 || rc == 0)
+		{
+			free(buf);
+			if (rc == 0)
+				return (SUCCESS);
+			else
+				return (ERROR);
+		}
+		buf[rc] = '\0';
+		*line = ft_strjoin(*line, buf);
+		free(buf);
+		if (ft_strchr(*line, '\n') != NULL)
+			return (SUCCESS);
+	}
+}
